64b6db994a11fcbbf6015a636931c357c41a9141
Alexander-Rice <alexander.h.rice@uconn.edu>
2023-12-14 20:50:35 -0500

    Added proper code to ratingchart in code

--- PaperContent/code/ratingchart.R
+++ PaperContent/code/ratingchart.R
@@ -1,8 +1,4 @@
-setwd("C:/Users/legoa/Downloads")
-videogames <- read.csv('Video_Games.csv')
-videogames$Rating <- as.factor(videogames$Rating)
-
-ratingcount <- table(videogames_no_NA$Rating)
+ratingcount <- table(videogamesfinal_no_NA$Rating)
 lbls <- paste(names(ratingcount), prettyNum(ratingcount,big.mark=","))
 pie(ratingcount, labels=lbls, col=rainbow(nrow(ratingcount)),
     main="ESRB Rating Counts", cex=0.8)
